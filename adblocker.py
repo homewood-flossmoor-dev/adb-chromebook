6f851d957ff02344c38b306c35b40809cb66a942
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-12-19 19:59:57 +0000

    script v1 with pyderman

--- adblocker.py
+++ adblocker.py
@@ -1,1 +1,30 @@
-import random
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+import pyderman
+
+driverpath = pyderman.install(browser=pyderman.chrome)
+driver = webdriver.Chrome(driverpath)
+
+driver.maximize_window()
+weburl = "https://www.youtube.com"
+driver.get(weburl)
+
+wait = ui.WebDriverWait(driver, 3000)
+
+while True:
+    try:
+        if EC.presence_of_element_located((By.XPATH,".//div/div/div/div/div/span/button/div[contains(text(),'Skip Ad')]")):
+            button = driver.find_element(By.XPATH, ".//div/div/div/div/div/span/button/div[contains(text(),'Skip Ad')]")
+            driver.execute_script("arguments[0].click();", button)
+            print("ad skipped")
+            time.sleep(2)
+        else:
+            continue
+
+    except NoSuchElementException:
+        print("no element detected")
+        time.sleep(2)
